bbca1282393247e8f7c741652a791b95e16d58cc
Shantanu Sutar <shantanusutar17@gmail.com>
2023-02-25 22:37:55 +0530

    header and its styling

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Header.jsx";
+import Home from "./components/Home.jsx";
 
 function App() {
-  return <div className="App">working</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
